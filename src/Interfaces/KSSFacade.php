191f2bcb4cba014a0db7fc1a212a0865fa6f87ca
ypilipenko <porfirovskiy@gmail.com>
2020-04-18 18:37:51 +0300

    add KSS facade

---
+++ src/Interfaces/KSSFacade.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Date: 18.04.20
+ * Time: 18:35
+ */
+
+namespace KnowledgeStorageSystem;
+
+
+class KSSFacade
+{
+
+    /**
+     * Routing users commands to project, part, part content
+     * and return results
+     */
+
+}
